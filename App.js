4f693b3206a3ca25efc7cb5b393edd7d9cf6e6c5
Pete <peteburquette@gmail.com>
2022-11-12 20:39:53 +0000

    font intialization for faster render

--- App.js
+++ App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import AmbientPlayer from './component/AmbientPlayer';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const App = () => {
+  console.log('***Loading Fonts***');
+  Ionicons.loadFont();
+  MaterialCommunityIcons.loadFont();
+
   return (
     <View style={styles.container}>
       <AmbientPlayer />
