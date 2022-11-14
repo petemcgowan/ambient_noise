169bc1a7e40a147bc9aa5adf2b6a2c213d8e36dd
Pete <peteburquette@gmail.com>
2022-11-14 16:53:03 +0000

    splash screen and cleanup

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, View} from 'react-native';
 
 import AmbientPlayer from './component/AmbientPlayer';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
+import SplashScreen from 'react-native-splash-screen';
+
 const App = () => {
   console.log('***Loading Fonts***');
   Ionicons.loadFont();
   MaterialCommunityIcons.loadFont();
+
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
   return (
     <View style={styles.container}>
