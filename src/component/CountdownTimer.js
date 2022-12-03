41253d97396a178b602efcb4dc740ae596831408
Pete <peteburquette@gmail.com>
2022-12-03 15:34:34 +0000

    componentizing entire app

---
+++ src/component/CountdownTimer.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+import {CountdownCircleTimer} from 'react-native-countdown-circle-timer';
+import {Animated} from 'react-native';
+
+const formatRemainingTime = countDownInSeconds => {
+  // const minutes = Math.floor((time % 3600) / 60);
+  // const seconds = time % 60;
+  if (countDownInSeconds === 0) {
+    return '0:00';
+  }
+  // const days = Math.floor(countDownInSeconds / (1000 * 60 * 60 * 24));
+  // const hours = Math.floor(
+  //   (countDownInSeconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  // );
+  const minutes = Math.floor(
+    (countDownInSeconds % (1000 * 60 * 60)) / (1000 * 60),
+  );
+  console.log('countDownInSeconds:' + countDownInSeconds);
+  const seconds = Math.floor((countDownInSeconds % (1000 * 60)) / 1000);
+  console.log('seconds:' + seconds);
+
+  const minutesMethod2 = Math.floor((countDownInSeconds % 3600) / 60);
+  console.log('minutesMethod2:' + minutesMethod2);
+  const secondsMethod2 = countDownInSeconds % 60;
+  console.log('secondsMethod2:' + secondsMethod2);
+
+  // const result = time.toLocaleDateString('en-GB', {
+  //   // you can use undefined as first argument
+  //   // year: "numeric",
+  //   // month: "2-digit",
+  //   // day: "2-digit",
+  //   minimumIntegerDigits: 2,
+  //   useGrouping: false,
+  // });
+  // console.log('paddingResult:' + result); // outputs “01/03/2018”
+
+  let secondString = seconds;
+  if (seconds < 10) {
+    secondString = 0 + seconds;
+  } else {
+    secondString = seconds;
+  }
+  console.log('secondString:' + secondString);
+
+  // return `${minutes}:${seconds}`;
+  return `${minutesMethod2}:${secondsMethod2}`;
+};
+
+// const renderTime = ({remainingTime}) => {
+//   if (remainingTime === 0) {
+//     return <View style={styles.timer}>Too late...</View>;
+//   }
+
+//   return (
+//     <View style={styles.timer}>
+//       {/* <View style={styles.text}>Remaining time</View> */}
+//       <View style={styles.value}>{formatRemainingTime(remainingTime)}</View>
+//     </View>
+//   );
+// };
+
+export default function CountdownTimer() {
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={124}
+      size={200}
+      updateInterval={1}
+      strokeWidth={18}
+      colors={[
+        '#9CDE9F',
+        '#D1F5BE',
+        '#3C91E6',
+        '#9FD356',
+        '#342E37',
+        '#FA824C',
+        '#4B3B40',
+        '#82735C',
+        '#9DB17C',
+      ]}
+      onComplete={() => {
+        console.log('ON_COMPLETE BEFORE RETURN');
+        return [true, 0];
+      }}>
+      {({remainingTime, animatedColor}) => (
+        <Animated.Text style={{color: animatedColor}}>
+          {/* <View style={styles.timer}> */}
+          {/* <View style={styles.timerTextContainer}>
+              <Text style={styles.text}>Remaining</Text>
+              <Text style={styles.text}>time</Text>
+            </View> */}
+          {/* <View style={styles.value}> */}
+          <Text style={styles.text}>{formatRemainingTime(remainingTime)}</Text>
+          {/* <Text style={styles.text}>{renderTime}</Text> */}
+          {/* </View> */}
+          {/* </View> */}
+        </Animated.Text>
+      )}
+    </CountdownCircleTimer>
+  );
+}
+
+const styles = StyleSheet.create({
+  timerTextContainer: {
+    // color: '#ccc',
+    // fontSize: 18,
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    margin: 3,
+  },
+  timer: {
+    // fontFamily: "Montserrat";
+    // flexDirection: column;
+    alignItems: 'center',
+  },
+  text: {
+    color: '#ccc',
+    fontSize: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  // value: {
+  //   fontSize: 30,
+  // },
+});
---
+++ src/component/TimerControls.js
@@ -0,0 +1,121 @@
+import React, {useState} from 'react';
+import {SafeAreaView, View, Text, StyleSheet, Dimensions} from 'react-native';
+import {TouchableOpacity, Modal, Pressable, Alert} from 'react-native';
+import {TimePicker} from 'react-native-simple-time-picker';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const {width} = Dimensions.get('window');
+
+export default function TimerControls({timerVisible, setTimerVisible}) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+
+  const handleChange = (value: {hours: number, minutes: number}) => {
+    setHours(value.hours);
+    setMinutes(value.minutes);
+    console.log(
+      'value.hours' + value.hours + ', value.minutes:' + value.minutes,
+    );
+  };
+
+  return (
+    <View>
+      <SafeAreaView>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              {/* <Text style={styles.modalText}>Set Countdown Timer</Text> */}
+              <TimePicker
+                textColor={'black'}
+                value={{hours, minutes}}
+                onChange={handleChange}
+              />
+              <View style={styles.modalBottomButtons}>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => setModalVisible(false)}>
+                  <Text style={styles.textStyle}>Back</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.button, styles.buttonClose]}
+                  onPress={() => {
+                    setModalVisible(!modalVisible);
+                    setTimerVisible(true);
+                  }}>
+                  <Text style={styles.textStyle}>Confirm</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </SafeAreaView>
+      <View style={styles.bottomContainer}>
+        <View style={styles.bottomControls}>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <Ionicons name="timer-outline" size={90} color="#777777" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    borderTopColor: '#393E46',
+    borderTopWidth: 1,
+    width: width,
+    alignItems: 'center',
+    paddingVertical: 15,
+  },
+  bottomControls: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    width: '80%',
+  },
+  button: {
+    width: '50%',
+    borderRadius: 10,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    // backgroundColor: 'rgba(122, 158, 199, 1)',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    // backgroundColor: '#393E46',
+    // borderWidth: 1,
+  },
+  modalView: {
+    marginTop: 80,
+    // backgroundColor: 'rgba(122, 158, 199, 1)',
+    backgroundColor: 'rgba(255, 255, 255, 1)',
+    // width: '80%',
+    borderRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 2,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalBottomButtons: {
+    flexDirection: 'row',
+    width: '100%',
+  },
+  textStyle: {
+    color: '#777777',
+    textAlign: 'center',
+  },
+});
